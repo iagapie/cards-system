24d0fd8285abfa5ff7d1145b739f5007924e9deb
Igor Agapie <igoragapie@gmail.com>
2021-06-04 05:44:50 +0300

    fixed get category api

--- src/services/category/src/Api/Application/Query/CategoryQuery.php
+++ src/services/category/src/Api/Application/Query/CategoryQuery.php
@@ -10,6 +10,8 @@ use Doctrine\DBAL\Connection;
 use JsonSerializable;
 
 use function array_filter;
+use function array_key_exists;
+use function array_merge;
 use function explode;
 
 final class CategoryQuery implements CategoryQueryInterface
@@ -31,45 +33,46 @@ final class CategoryQuery implements CategoryQueryInterface
     public function getCategory(string $id): JsonSerializable
     {
         $sql = '
-            SELECT c.*, GROUP_CONCAT(p.permission SEPARATOR \' \') as permissions
+            SELECT
+                c.id,
+                c.parent_id,
+                c.board_id,
+                c.created_by,
+                c.name,
+                c.description,
+                c.position,
+                c.created_at,
+                c.updated_at,
+                GROUP_CONCAT(p.permission SEPARATOR \' \') as permissions
             FROM '.self::CATEGORIES.' c
             LEFT JOIN '.self::PERMISSIONS.' p ON p.category_id = c.id
-            WHERE c.id = :id
+            WHERE c.id = :id OR c.parentId = :id
             GROUP BY c.id
-            LIMIT 1
         ';
 
-        if ($row = $this->connection->fetchAssociative($sql, ['id' => $id])) {
-            return new class($row) implements JsonSerializable {
-                public function __construct(private array $row)
-                {
-                }
+        $rows = $this->connection->fetchAllAssociativeIndexed($sql, ['id' => $id]);
 
-                public function jsonSerialize()
-                {
-                    $ca = new DateTimeImmutable($this->row['created_at']);
-                    $ua = new DateTimeImmutable($this->row['updated_at']);
-
-                    $permissions = explode(' ', $this->row['permissions'] ?? '');
-                    $permissions = array_filter($permissions);
-
-                    return [
-                        'id' => $this->row['id'],
-                        'parentId' => $this->row['parent_id'] ?: null,
-                        'boardId' => $this->row['board_id'],
-                        'createdBy' => $this->row['created_by'],
-                        'name' => $this->row['name'],
-                        'description' => $this->row['description'] ?: null,
-                        'position' => (int)$this->row['position'],
-                        'createdAt' => $ca->format('c'),
-                        'updatedAt' => $ua->format('c'),
-                        'permissions' => $permissions,
-                    ];
-                }
-            };
+        if (count($rows) === 0 || !array_key_exists($id, $rows)) {
+            throw new RecordNotFoundException();
         }
 
-        throw new RecordNotFoundException();
+        $row = $this->normalize($rows[$id], ['id' => $id, 'children' => []]);
+        unset($rows[$id]);
+
+        foreach ($rows as $key => $value) {
+            $row['children'][$key] = $this->normalize($value, ['id' => $key]);
+        }
+
+        return new class($row) implements JsonSerializable {
+            public function __construct(private array $row)
+            {
+            }
+
+            public function jsonSerialize(): array
+            {
+                return $this->row;
+            }
+        };
     }
 
     /**
@@ -78,10 +81,37 @@ final class CategoryQuery implements CategoryQueryInterface
     public function getCategories(int $skip, int $limit): JsonSerializable
     {
         return new class implements JsonSerializable {
-            public function jsonSerialize()
+            public function jsonSerialize(): array
             {
                 return [];
             }
         };
+    }
+
+    /**
+     * @param array $row
+     * @param array $append
+     * @return array
+     */
+    private function normalize(array $row, array $append): array
+    {
+        $ca = new DateTimeImmutable($row['created_at']);
+        $ua = new DateTimeImmutable($row['updated_at']);
+
+        $permissions = explode(' ', $row['permissions'] ?? '');
+        $permissions = array_filter($permissions);
+
+        return array_merge(
+            $row,
+            [
+                'parentId' => $row['parent_id'] ?: null,
+                'description' => $row['description'] ?: null,
+                'position' => (int)$row['position'],
+                'createdAt' => $ca->format('c'),
+                'updatedAt' => $ua->format('c'),
+                'permissions' => $permissions,
+            ],
+            $append
+        );
     }
 }
